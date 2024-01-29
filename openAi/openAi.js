0b0df71ac3016f95cdea09117829ca2aaa4852c7
chewbacca234 <carine.dupuis.es@gmail.com>
2024-01-29 10:29:30 +0100

    create story beginning

---
+++ openAi/openAi.js
@@ -0,0 +1,124 @@
+const OpenAI = require('openai');
+
+const openai = new OpenAI();
+const model = 'gpt-3.5-turbo-1106';
+
+async function charactersCreation(players) {
+  const messages = [
+    {
+      role: 'system',
+      content: `Tu es mon assistant game-master qui connaît sur le bout des doigts l'univers de donjon et dragon.`,
+    },
+    {
+      role: 'user',
+      content: `Pour chaque joueur de cette liste : ${players}, crée un fichier json avec un index partant de 0, un name: nom fourni dans la liste, un character: invente nom du personnage dans le jeu et une description: invente un résumée de ses caractéristiques.`,
+    },
+  ];
+
+  try {
+    const completion = await openai.chat.completions.create({
+      model,
+      messages,
+      response_format: { type: 'json_object' },
+      max_tokens: 500,
+      temperature: 0.7,
+    });
+
+    const response = completion.choices[0];
+    const characters = JSON.parse(response.message.content).joueurs;
+
+    console.log('[BACKEND][OPENAI] created characters:', characters);
+    return characters;
+  } catch (error) {
+    console.error('[BACKEND][OPENAI] create characters error:', error);
+  }
+}
+
+/* 
+Return example :
+[
+    {
+        "index": 0,
+        "name": "Jean",
+        "character": "Eldrin le sorcier",
+        "description": "Eldrin est un sorcier puissant, maître des éléments et manipulateur de la magie noire. Il est aussi connu pour son intelligence et sa ruse, ce qui en fait un redoutable adversaire dans les donjons."
+    },
+    {
+        "index": 1,
+        "name": "Viviane",
+        "character": "Sylanna l'elfe archère",
+        "description": "Sylanna est une archère agile et précise, capable de tirer des flèches mortelles à une distance impressionnante. Elle possède également une grande connaissance des plantes et des herbes, utile pour soigner ses compagnons."
+    },
+    {
+        "index": 2,
+        "name": "Marie",
+        "character": "Gorim le guerrier nain",
+        "description": "Gorim est un guerrier nain redoutable, armé d'une hache gigantesque et protégé par une armure indestructible. Sa force et sa résistance en font un combattant redoutable, prêt à affronter les dangers les plus mortels des donjons."
+    }
+]
+*/
+
+async function storyBeginningCreation(
+  charactersDescription,
+  univers,
+  storyLength,
+  style
+) {
+  const messages = [
+    {
+      role: 'system',
+      content: `Tu es mon assistant game-master qui connaît sur le bout des doigts l'univers de donjon et dragon. Tu devras créer l'histoire du jeu de rôles en fonction des informations que je vais te fournir, cette histoire contiendra ${storyLength} phases de jeu et se déroulera dans plutôt en ${style}. Chacune de ces phases contiendra un résumé de l'histoire en cours, posant le contexte pour les joueurs, et 3 choix que tu proposera à chaque joueur en fonction de leurs caractéristiques et de leur avancée dans le jeu.`,
+    },
+    {
+      role: 'user',
+      content: `Crée le début de l'histoire dans l'univers de la ${univers}. Il y a ${charactersDescription.length} joueurs. La description des personnages est : ${charactersDescription}. tu renverras un json avec un title : le titre de l'histoire, un storyBeginning : le début de l'histoire assez détaillé, et des choices : un tableau contenant 3 choix pour chaque joueur. Sois inventif !`,
+    },
+  ];
+
+  try {
+    const completion = await openai.chat.completions.create({
+      model,
+      messages,
+      response_format: { type: 'json_object' },
+      max_tokens: 2000,
+      temperature: 0.4,
+    });
+
+    const response = completion.choices[0];
+    const storyBeginning = JSON.parse(response.message.content);
+
+    console.log('[BACKEND][OPENAI] created story beginning:', storyBeginning);
+    return storyBeginning;
+  } catch (error) {
+    console.error('[BACKEND][OPENAI] create story beginning error:', error);
+  }
+}
+
+/* 
+Return example :
+{
+    "title": "Les Montagnes Maudites",
+    "storyBeginning": "Les Montagnes Maudites s'élèvent majestueusement devant vous, leurs sommets enneigés cachant d'anciens donjons et des créatures redoutables. Vous avez été recrutés pour explorer ces terres dangereuses et récupérer un artefact magique convoité par de nombreux aventuriers. Alors que vous vous approchez des montagnes, un blizzard féroce se lève, obscurcissant le ciel et rendant la visibilité presque nulle. Vous entendez des grondements sinistres provenant des profondeurs des montagnes, signe que de redoutables créatures rôdent dans les sombres cavernes. Vous savez que l'artefact se trouve quelque part au cœur de ces montagnes, mais pour y parvenir, vous devrez affronter des dangers inimaginables.",
+    "choices": {
+        "Eldrin": {
+            "choice1": "Utiliser vos pouvoirs magiques pour créer un bouclier de protection contre le blizzard et les éléments déchaînés.",
+            "choice2": "Lancer un sort de détection pour repérer d'éventuels pièges magiques ou des créatures cachées dans les environs.",
+            "choice3": "Invoquer les éléments pour tenter de calmer le blizzard et ouvrir un chemin sûr à travers les montagnes."
+        },
+        "Sylanna": {
+            "choice1": "Utiliser vos compétences en survie pour trouver un abri temporaire et protéger le groupe du blizzard.",
+            "choice2": "Utiliser vos connaissances des plantes pour trouver des herbes médicinales capables de soigner les effets du froid extrême sur le groupe.",
+            "choice3": "Utiliser votre arc pour repérer d'éventuelles menaces cachées dans le blizzard et prévenir le groupe de tout danger imminent."
+        },
+        "Gorim": {
+            "choice1": "Utiliser votre force pour ouvrir un passage à travers la neige et le vent, créant un chemin sûr pour le groupe.",
+            "choice2": "Inspecter les environs à la recherche de traces de créatures dangereuses et préparer le groupe à toute éventualité.",
+            "choice3": "Utiliser votre hache pour couper du bois et construire un abri solide pour protéger le groupe du blizzard."
+        }
+    }
+}
+*/
+
+async function continueStory(context) {}
+
+module.exports = { charactersCreation, storyBeginningCreation, continueStory };
--- routes/stories.js
+++ routes/stories.js
@@ -2,11 +2,57 @@ var express = require('express');
 var router = express.Router();
 const Story = require('../models/Story');
 const User = require('../models/User');
+const {
+  charactersCreation,
+  storyBeginningCreation,
+} = require('../openAi/openAi');
 
-/* Save a new story. (save the story in stories collection & save datas in inProgressStories of gameMaster user db) */
+/* Create player's characters. */
+router.get('/characters/', async (req, res) => {
+  const players = req.query.players;
+  // console.log('players:', players);
+
+  try {
+    const createdCharacters = await charactersCreation(players);
+
+    console.log('[BACKEND][STORIES] created characters:', createdCharacters);
+    res.json(createdCharacters);
+  } catch (error) {
+    console.error('[BACKEND][STORIES] create characters error:', error);
+    res.status(400).json({ error });
+  }
+});
+
+/* Create story's beginning. */
+router.get('/beginning', async (req, res) => {
+  const { charactersDescription, univers, storyLength, style } = req.query;
+  // console.log('charactersDescription:', charactersDescription);
+  // console.log('univers:', univers);
+  // console.log('storyLength:', storyLength);
+  // console.log('style:', style);
+
+  try {
+    const storyBeginning = await storyBeginningCreation(
+      charactersDescription,
+      univers,
+      storyLength,
+      style
+    );
+
+    console.log(
+      "[BACKEND][STORIES] created story's beginning:",
+      storyBeginning
+    );
+    res.json(storyBeginning);
+  } catch (error) {
+    console.error("[BACKEND][STORIES] create story's beginning error:", error);
+    res.status(400).json({ error });
+  }
+});
+
+/* Create a new story. (create the story in stories collection & add datas in inProgressStories of gameMaster user db) */
 router.post('/', async (req, res) => {
-  const { token, gameMaster, players, title, turn, playerTurn, context } =
-    req.body;
+  const { token, gameMaster, players, title, context } = req.body;
   const lastTimePlayed = new Date();
 
   // Check user token
@@ -16,36 +62,48 @@ router.post('/', async (req, res) => {
     return res.status(400).json({ error: 'User not found' });
   }
 
+  // Check if story's title already exists
+  let newtitle;
+  titleExists = await Story.find({ title: { $regex: title } });
+  if (titleExists.length === 0) {
+    newtitle = title;
+  } else {
+    newtitle = title + ` #${titleExists.length + 1}`;
+  }
+
   // Save story to DB
   try {
-    savedStory = await Story.create({
+    createdStory = await Story.create({
       gameMaster,
       players,
-      title,
+      title: newtitle,
       lastTimePlayed,
-      turn,
-      playerTurn,
+      turn: 0,
+      playerTurn: 0,
       context,
     });
 
     // Add story to user's in progress stories list
-    if (savedStory) {
+    if (createdStory) {
       await User.updateOne(
         { token },
         {
           $push: {
             inProgressStrories: {
-              title,
-              lastTimePlayed,
-              story: savedStory._id,
+              title: createdStory.newtitle,
+              lastTimePlayed: createdStory.lastTimePlayed,
+              story: createdStory._id,
             },
           },
         }
       );
     }
+
+    console.error('[BACKEND] DB created story:', createdStory);
+    return res.json({ createdStory });
   } catch (error) {
     // Log & return error message
-    console.error('[BACKEND] DB saving story error', error);
+    console.error('[BACKEND] DB create story error:', error);
     return res.status(400).json({ error });
   }
 });
